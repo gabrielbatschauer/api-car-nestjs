e9a0de7669c81c217eff4f563cc61162d25b65c4
Gabriel <gbatschauer@gmail.com>
2025-07-11 12:19:27 -0300

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,51 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestjsSwagger } from '@anatine/zod-nestjs';
+import { ZodExceptionFilter } from './filters/zod-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ZodExceptionFilter());
+
+  patchNestjsSwagger();
+  const config = new DocumentBuilder()
+    .setTitle('API Gestão de Estoque Veicular')
+    .setDescription(
+      'API com validação JWT para CRUD completo de estoque de carros',
+    )
+    .setDescription(
+      `
+Esta API permite que lojistas gerenciem seus estoques de veículos com segurança e autenticação via JWT.
+
+### Como usar:
+
+1. **Crie um usuário** com \`POST /user\`
+2. **Faça login** com \`POST /login\` e receba o token JWT
+3. **Clique em "Authorize" no Swagger** e cole o token fornecido pelo passo 2
+4. Agora você pode acessar rotas protegidas como \`POST /vehicles\`, \`GET /vehicles\`, etc.
+
+🔒 **Todas as rotas de veículos exigem autenticação.**
+ **Cada usuário só acessa os veículos que ele criou.**
+
+---
+`,
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'jwt',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
